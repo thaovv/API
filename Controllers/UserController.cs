5f789d2c4269c9238b28cb2a7f83ea13a699bd82
thaovv <vuvanthao2408.nd@gmail.com>
2024-03-18 17:41:48 +0700

    end4

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,13 +1,13 @@
 ﻿using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-public class UsersController : ControllerBase
+
+public class UsersController : BAseApiController
 {
     private readonly DataContext _context;
 
@@ -17,11 +17,13 @@ public class UsersController : ControllerBase
     }
 
     [HttpGet]
+    [AllowAnonymous]
     public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
     {
         return await _context.Users.ToListAsync();
     }
 
+    [Authorize]
     [HttpGet("{id}")]
     public async Task<ActionResult<AppUser>> GetUser(int id)
     {
