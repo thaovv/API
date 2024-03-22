41bda19904156054fe99ad6f10964112ad366b9b
thaovv <vuvanthao2408.nd@gmail.com>
2024-03-22 10:14:48 +0700

    end 9

--- Program.cs
+++ Program.cs
@@ -1,32 +1,24 @@
-// using API.Data;
-// using Microsoft.EntityFrameworkCore;
 using System.Text;
+using API.Errors;
 using API.Extensions;
+using API.Middleware;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// builder.Services.AddDbContext<DataContext>(opt => 
-// {
-//     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-// });
-// builder.Services.AddCors();
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
     .WithOrigins("https://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
